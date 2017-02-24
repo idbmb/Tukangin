77b1b5a6890a246eea807df7a71e453f583d10a0
Bambang Handoko <email": null>
2017-02-24 14:58:27 +0700

    update page

--- routes/tukang/index.js
+++ routes/tukang/index.js
@@ -3,10 +3,23 @@ var router = express.Router();
 const Model = require('../../models')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Model.Profile_tukang.findAll({include:[Model.Skill_tukang]}).then(function(tukangs){
+router.get('/:id', function(req, res, next) {
+  Model.Profile_tukang.findAll({include:[Model.Skill_tukang],where:{SkillTukangId : req.params.id}}).then(function(tukangs){
     res.render('tukang/index',{tukangs : tukangs})
   })
 });
+
+router.get('/hire/:id', function(req, res, next) {
+  Model.Profile_tukang.findById(req.params.id).then(function(tukang){
+    // Still use dummy data user, login not ready yet
+    Model.User.findById(req.params.id).then(function(user){
+      res.render('tukang/hire_tukang',{tukang : tukang, user: user})
+    })
+  })
+})
+
+router.post('/order', function(req, res, next) {
+  
+})
 
 module.exports = router;
